51f63667d3262c55991b26e3e6666c20ec358d26
blackweeed <blackweeeed@gmail.com>
2023-02-17 19:43:40 +0100

    center images

--- src/pages/PokemonPage.tsx
+++ src/pages/PokemonPage.tsx
@@ -29,6 +29,34 @@ const PokemonPage = () => {
           </p>
         </div>
       </div>
+      <div className="pokemon-detail__profile">
+        <div className="pokemon-main">
+          <div className="pokemon-main__center">
+            <div className="pokemon-img">
+              {
+                <img
+                  src="https://sg.portal-pokemon.com/play/resources/pokedex/img/pokemon_bg.png"
+                  alt=""
+                  className="pokemon-img__back"
+                />
+              }
+              {
+                <img
+                  src="https://sg.portal-pokemon.com/play/resources/pokedex/img/pokemon_circle_bg.png"
+                  alt=""
+                  className="pokemon-img__blur"
+                />
+              }
+              <img
+                src="https://sg.portal-pokemon.com/play/resources/pokedex/img/pm/3245e4f8c04aa0619cb31884dbf123c6918b3700.png"
+                alt=""
+                className="pokemon-img__front"
+              />
+            </div>
+          </div>
+          <div className="pokemon-main__right"></div>
+        </div>
+      </div>
     </div>
   );
 };
